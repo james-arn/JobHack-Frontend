c418f099d98406f63046b74d4328a62febdb264c
Ahmed <atomcgarry@gmail.com>
2021-11-30 18:27:14 +0000

    canban-board persists changes on card drop bug fixed

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,22 +1,29 @@
 import "./Column.css";
 import { JobCard } from "../JobCard/JobCard";
 import { Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
 
 export const Column = (props) => {
+
+  const Container = styled.div`
+  background-color: ${props => (props.isDraggingOver ? 'skyblue': 'white')}
+  `
   return (
     <div className="col-container">
       <h3 className="col-title">{props.column.title}</h3>
       <Droppable droppableId={props.column.id}>
-        {(provided) => (
-          <div 
+        {(provided, snapshot) => (
+          <Container 
             className="job-list"
             {...provided.droppableProps}
-            ref={provided.innerRef}>
+            ref={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+            >
             {props.jobs.map((job,index) => (
               <JobCard key={job.id} job={job} index={index} />
             ))}
             {provided.placeholder}
-          </div>
+          </Container>
         )}
       </Droppable>
     </div>
