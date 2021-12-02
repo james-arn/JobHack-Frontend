5fff0699d9b536fff3ec384f85bed7d049eb8f83
rainy-j <90918702+rainy-j@users.noreply.github.com>
2021-12-02 15:26:16 +0000

    fixed modal

--- src/components/styles/modal.styled.js
+++ src/components/styles/modal.styled.js
@@ -1,22 +1,27 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+import Modal from "react-modal"
 
 
-// export const ModalWrapper = styled.div`
+export const ModalWrapper = styled.div`
 
-// `
+`
 
-// export const CurrentDataContainer = styled.div`
+export const CurrentDataContainer = styled.div`
 
-// `
+`
 
-// export const TitleContainer = styled.div`
+export const TitleContainer = styled.div`
 
-// `
+`
 
-// export const DescriptionContainer = styled.div`
+export const DescriptionContainer = styled.div`
 
-// `
+`
 
-// export const ButtonContainer = styled.div`
+export const ButtonContainer = styled.div`
 
-// `
+`
+
+export const ModalCont = styled(Modal)`
+
+`
